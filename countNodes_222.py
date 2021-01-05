22d684a12f25ee6456ea20a766c453e670c69c74
zh.tong <zh.tong@idiaoyan.cn>
2021-01-05 17:48:21 +0800

    222 完全二叉树

---
+++ countNodes_222.py
@@ -0,0 +1,42 @@
+"""
+222. 完全二叉树的节点个数
+给出一个完全二叉树，求出该树的节点个数。
+说明：
+完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+示例:
+输入:
+    1
+   / \
+  2   3
+ / \  /
+4  5 6
+输出: 6
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from node import TreeNode
+
+
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        left_level = self.countLevel(root.left)
+        right_level = self.countLevel(root.right)
+        if left_level == right_level:  # 左子树填满，右子树不满
+            return 2 ** left_level + self.countNodes(root.right)
+        if left_level > right_level:  # 左子树不满，右子树填满
+            return self.countNodes(root.left) + 2 ** right_level
+
+    def countLevel(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        level = 0
+        while root:
+            level += 1
+            root = root.left
+        return level
